ccc650ba311638c2055bfb6ccbc2520ff848128f
Can <candgrmc@gmail.com>
2018-09-24 15:41:28 +0300

    index

--- hello.py
+++ hello.py
@@ -166,7 +166,7 @@ class User(db.Model):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run( port=port)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=8000)
     db.create_all()
 
