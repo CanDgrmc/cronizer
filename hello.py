8bb8def89444beed7dcc0ab7edecf4696548abb0
Can <candgrmc@gmail.com>
2018-09-24 14:07:42 +0300

    initial

---
+++ hello.py
@@ -0,0 +1,166 @@
+from flask import Flask, \
+    request, \
+    render_template,\
+    make_response,\
+    jsonify,\
+    flash,\
+    redirect,\
+    url_for,\
+    session,\
+    logging
+
+from flask_restful import Resource,Api,reqparse
+from flask_mysqldb import MySQL
+from flask_sqlalchemy import SQLAlchemy
+import json
+import hashlib
+import requests
+from pytz import timezone
+import pytz
+
+
+import random
+import string
+from datetime import datetime
+
+
+
+app = Flask(__name__)
+api = Api(app)
+db = SQLAlchemy(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/cronizer?'
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'cronizer'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql = MySQL(app)
+TODOS = {
+    'todo1': {'task': 'build an API'},
+    'todo2': {'task': '?????'},
+    'todo3': {'task': 'profit!'},
+}
+
+
+parser = reqparse.RequestParser()
+
+
+class logMeIn(Resource):
+    def post(self):
+
+        parser.add_argument('email')
+        parser.add_argument('password')
+        userInfo = parser.parse_args()
+
+        if userInfo.email :
+            token = validateUser(userInfo.email, userInfo.password)
+            if token :
+                return {'success': True, 'message': 'Login Successful' ,'token': token},201
+            return { 'success': False, 'message': 'Wrong Credentials'},403
+
+
+
+
+
+api.add_resource(logMeIn,'/login')
+
+
+
+class crons(Resource):
+    def post(self):
+        parser.add_argument('token')
+        parser.add_argument('json')
+        parser.add_argument('columns')
+        parser.add_argument('error_message')
+        parser.add_argument('status')
+        postData = parser.parse_args();
+
+
+        tz = pytz.timezone('Europe/Istanbul')
+        turkey = datetime.now(tz)
+        return str(turkey)
+
+        now_turkey = aware_date.astimezone(turkey)
+        return now_turkey
+
+        user = validateToken(postData.token)
+        if user:
+            status = False
+            if postData.status == "1":
+                status = True
+            else:
+                sendMail(user.email,postData.error_message)
+            d = datetime.now()
+            timezone = pytz.timezone("Europe/Istanbul")
+            d_aware = timezone.localize(d)
+
+            cron = Cron(user_id = user.id, json = postData.json, columns = postData.columns,created_at = datetime.now(),status = status )
+            db.session.add(cron)
+            db.session.commit()
+            if cron.id:
+                return {'success': True, 'data': cron.json, 'created_at': str(cron.created_at)}, 201
+
+        return {'success': False, 'message': 'Check your post data / credentials'}, 403
+
+api.add_resource(crons, '/push')
+
+
+
+
+
+
+
+
+def id_generator(size=6, chars=string.ascii_letters + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+def validateUser(email,password):
+    user = User.query.filter_by(email=email).first()
+    if user:
+        m = hashlib.sha224(password.encode()).hexdigest()
+        if (m == user.password):
+            return user.token
+        return False
+
+def validateToken(token):
+    user = User.query.filter_by(token= token).first()
+    return user
+
+def sendMail(to,content):
+    key = 'key-44571fbb1ac9be648dfdc1c36618fedc'
+    sandbox = 'onlals.at'
+
+    request_url = 'https://api.mailgun.net/v2/{0}/messages'.format(sandbox)
+    request = requests.post(request_url, auth=('api', key), data={
+        'from': 'noreply@onlals.at',
+        'to': to,
+        'subject': 'Hatalı Cron',
+        'html': render_template('mail/send.html',content = content)
+    })
+
+    print
+    'Status: {0}'.format(request.status_code)
+    print
+    'Body:   {0}'.format(request.text)
+
+class Cron(db.Model):
+    id = db.Column(db.Integer , primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False)
+    json = db.Column(db.Text, nullable=False)
+    columns = db.Column(db.Text,nullable = True)
+    created_at = db.Column(db.TIMESTAMP,nullable=False)
+    status = db.Column(db.Boolean,nullable=False)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100),nullable=False)
+    password = db.Column(db.String(100),nullable=False)
+    email = db.Column(db.String(100),nullable=False)
+    token = db.Column(db.Text,nullable=False)
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run(debug= True);
+
