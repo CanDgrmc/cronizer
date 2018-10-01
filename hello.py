1cc72f6070ef64506ff54c36b71da661ecb227e3
Can Dgrmc <candgrmc@gmail.com>
2018-10-01 15:00:57 +0300

    get

--- hello.py
+++ hello.py
@@ -181,6 +181,6 @@ class Project(db.Model):
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8000))
-    app.run(host='127.0.0.1', port=port,debug=True)
+    app.run(host='0.0.0.0', port=port)
     db.create_all()
 
